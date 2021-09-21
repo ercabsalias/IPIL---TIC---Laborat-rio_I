c54aca5dbc824483735bc1c41a0c006e0a57e42c
Ernesto Cabingano Salias <ernesto.cabingano.salias@gmail.com>
2021-09-21 22:41:41 +0100

    Uma pequena mudança

--- exer1.c
+++ exer1.c
@@ -2,10 +2,13 @@
 
 int main()
 {
-    float lado=0, A=0;
-    printf("Informe o lado: ");
-    scanf("%f", &lado);
-    A = lado*lado;
+    float lado1=0, lado2=0, A=0;
+    printf("Informe o 1 lado: ");
+    scanf("%f", &lado1);
+    
+    printf("Informe o 2 lado: ");
+    scanf("%f", &lado2);
+    A = lado1*lado2;
     printf("A área do quadrado = %f", A);
     return 0;
 }
